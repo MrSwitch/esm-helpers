6a859f34cda3526e89fe3394ebde35f760828537
Andrew Dodson <andrew@5app.com>
2021-06-30 11:15:04 +0100

    docs(jsdoc): add jsdoc

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ import fs from 'fs';
 import {join, dirname} from 'path';
 import {fileURLToPath} from 'url';
 
+/**
+ * requireJSON
+ * @param {object} meta - esm import.meta object
+ * @param {string} relativePath - file path
+ * @returns {*} JSON Content
+ */
 export function requireJSON(meta, relativePath) {
 	const __dirname = dirName(meta);
 
 	return JSON.parse(fs.readFileSync(join(__dirname, relativePath)));
 }
 
+/**
+ * @param {object} meta - esm import.meta object
+ * @returns {string} Directory name
+ */
 export function dirName(meta) {
 	const __filename = fileURLToPath(meta.url);
 	return dirname(__filename);
